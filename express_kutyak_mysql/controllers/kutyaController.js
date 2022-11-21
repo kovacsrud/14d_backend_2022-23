dd7feb17dc6dc10bb0d0729ea10786d24686c16a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-11-21 09:11:19 +0100

    Kutyak kontroller teljes funkcionalitás

--- express_kutyak_mysql/controllers/kutyaController.js
+++ express_kutyak_mysql/controllers/kutyaController.js
@@ -16,8 +16,49 @@ const getKutyak=(req,res)=>{
     })
 }
 
+const postKutya=(req,res)=>{
+    const {nevid,fajtaid,eletkor,utolsoell}=req.body;
+    conn.query("INSERT INTO kutyak (nevid,fajtaid,eletkor,utolsoell) values(?,?,?,?)"
+    ,[nevid,fajtaid,eletkor,utolsoell]
+    ,(err,result)=>{
+        if(err){
+            res.status(400).send(err);
+        } else {
+            res.json({message:"Adat beszúrva!"+result.affectedRows});
+        }
+    })
+}
+
+const patchKutya=(req,res)=>{
+    const {id,nevid,fajtaid,eletkor,utolsoell}=req.body;
+    conn.query("UPDATE kutyak SET nevid=?,fajtaid=?,eletkor=?,utolsoell=? WHERE id=?"
+    ,[nevid,fajtaid,eletkor,utolsoell,id]
+    ,(err,result)=>{
+        if(err){
+            res.status(400).send(err);
+        } else {
+            res.json({message:"Adat módosítva!"+result.affectedRows});
+        }
+    })
+}
+
+const deleteKutya=(req,res)=>{
+    const {id}=req.body;
+    conn.query("DELETE FROM kutyak WHERE id=?"
+    ,[id]
+    ,(err,result)=>{
+        if(err){
+            res.status(400).send(err);
+        } else {
+            res.json({message:"Adat törölve!"+result.affectedRows});
+        }
+    })
+}
+
 module.exports={
-    getKutyak
+    getKutyak,
+    postKutya,
+    patchKutya,deleteKutya
 }
 
 //SELECT kutyak.id,fajtaid,nev,kutyanev, nevid, eletkor, utolsoell FROM kutyak,kutyanevek,kutyafajtak WHERE kutyak.nevid=kutyanevek.id and kutyak.fajtaid=kutyafajtak.id 
