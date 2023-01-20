7d6062b57d56b80e1edefb5aa637c2d1570c0758
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-01-20 09:21:36 +0100

    Login

--- user_login_backend/controllers/userController.js
+++ user_login_backend/controllers/userController.js
@@ -8,7 +8,20 @@ const register=asyncHandler(async (req,res)=>{
 });
 
 const login=asyncHandler(async (req,res)=>{
-    res.send("Login");
+    const{username,password}=req.body;
+
+    const user=await User.findOne({username:username});
+    if(!user){
+        res.status(400);
+        throw new Error("Nincs ilyen felhasználó!");
+    }
+    if(!await bcrypt.compare(password,user.password)){
+        res.status(400);
+        throw new Error("Hibás jelszó!");
+    }
+
+    res.status(200).json(user);
+
 });
 
 const getUser=asyncHandler(async (req,res)=>{
